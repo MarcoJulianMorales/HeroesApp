5b6d58276755f8fdc499804ba3f6f434c36cbe7f
Marco Julian Morales <marcojulian.morales@gmail.com>
2023-08-02 13:51:39 -0700

    Terminada seccion 16, guards

--- src/app/heroes/pages/layout-page/layout-page.component.ts
+++ src/app/heroes/pages/layout-page/layout-page.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from 'src/app/auth/interfaces/user.interface';
+import { AuthService } from 'src/app/auth/services/auth.service';
 
 @Component({
   selector: 'app-layout-page',
@@ -12,4 +15,15 @@ export class LayoutPageComponent {
     {label: 'Add', icon: 'add', url: './add'},
     {label: 'Search', icon: 'search', url: './search'},
   ]
+
+  constructor(private authService: AuthService, private router: Router){}
+
+  get user(): User | undefined {
+    return this.authService.currentuser;
+  }
+
+  onLogout(){
+    this.authService.logout();
+    this.router.navigate(['/auth/login'])
+  }
 }
